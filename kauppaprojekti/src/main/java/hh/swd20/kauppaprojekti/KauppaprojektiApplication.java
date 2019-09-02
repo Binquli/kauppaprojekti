b2a24970bf085234037d0ec94a6b8181a2ca475e
Joonas <bgg306@myy.haaga-helia.fi>
2019-09-02 09:50:45 +0300

    first changes

---
+++ kauppaprojekti/src/main/java/hh/swd20/kauppaprojekti/KauppaprojektiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.kauppaprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauppaprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauppaprojektiApplication.class, args);
+	}
+
+}
